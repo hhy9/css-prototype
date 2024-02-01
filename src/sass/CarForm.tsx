dc6986edca5e49b991657a60384115c3bfab683c
hhy9 <154538@naver.com>
2024-02-01 11:37:59 +0900

    first

---
+++ src/sass/CarForm.tsx
@@ -0,0 +1,39 @@
+// import React from "react";
+import './CarForm.scss';
+
+const CarForm = () => {
+  return (
+    <>
+     <div className="car-form-layout">
+        <div className="car-form-card">
+          <div className="card-title">
+            <span>차량 등록/수정</span>
+          </div>
+          <div className="car-form-body">
+            <div className="form-flex">
+              <label className="form-label">차량번호</label>
+              <input />
+            </div>
+            <div className="form-flex">
+              <label className="form-label">차량구분</label>
+              <input />
+            </div>
+            <div className="form-flex">
+              <label className="form-label">차량소유자</label>
+              <div>
+                <input />
+              </div>
+            </div>
+            <div className="form-flex">
+              <label className="form-label">차량유형</label>
+              <input />
+            </div>
+          </div>
+        </div>
+        <div>차량 목록</div>
+      </div>
+    </>
+  );
+};
+
+export default CarForm;
---
+++ src/sass/PlaceForm.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * 장소 등록 / 수정 페이지
+ * @returns 
+ */
+const PlaceForm = () => {
+    return (
+        <>
+        
+        </>
+    )
+};
+
+export default PlaceForm;
